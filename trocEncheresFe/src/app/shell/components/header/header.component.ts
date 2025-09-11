cd54ac4d99bb6501af9e323fb5b80f4d4e92a4b0
Huby Franck <52332+symio@users.noreply.github.com>
2025-09-11 16:12:25 +0200

    Projet Web, TrocEnchères : Adapatation du front : WIP auth logout link

--- trocEncheresFe/src/app/shell/components/header/header.component.ts
+++ trocEncheresFe/src/app/shell/components/header/header.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+// src/app/shell/components/header/header.component.ts
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LanguageSelectorComponent } from '@app/i18n';
-import { UntilDestroy } from '@ngneat/until-destroy';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { OAuth2Service } from '@app/@core/services/oauth2.service';
 
 @UntilDestroy()
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [ LanguageSelectorComponent, CommonModule ],
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    standalone: true,
+    imports: [LanguageSelectorComponent, CommonModule],
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  menuHidden = true;
+export class HeaderComponent implements OnInit {
+    menuHidden = true;
+    isAuthenticated = false;
+
+    constructor(private oauth2Service: OAuth2Service) {
+        this.isAuthenticated$ = this.oauth2Service.isAuthenticated$;
+    }
 }
